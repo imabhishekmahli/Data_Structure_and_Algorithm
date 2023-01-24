a3f85c8f9eb0f280dad01935cabcdc34ce09795e
imabhishekmahli <abhi8h3k@gmail.com>
2023-01-24 23:52:50 +0530

    preorder

--- tree/binarytree.cpp
+++ tree/binarytree.cpp
@@ -15,6 +15,15 @@ struct node {
     }
 };
 
+void preorder(struct node *root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
 int main()
 {
     struct node *root = new node(1);
@@ -29,6 +38,10 @@ int main()
     
     root->left->left = new node(4);
     root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    preorder(root);
 
     return 0;
 }
