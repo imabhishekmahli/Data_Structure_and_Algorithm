39dc8cb2ee2ae0350c4f1b860c5299fad984f474
imabhishekmahli <abhi8h3k@gmail.com>
2024-02-28 14:32:53 +0530

    pattern ask row and col

---
+++ 2024/PatternAskRowCol.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+    int col;
+    cin>>row;
+    cin>>col;
+
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
