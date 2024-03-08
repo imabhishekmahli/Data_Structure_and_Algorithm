fa3c2780fff222f1c7b05d17d611f3259d0041c9
imabhishekmahli <abhi8h3k@gmail.com>
2024-03-08 11:27:32 +0530

    function

---
+++ 2024/Functions/printFun.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void printfun()
+{
+    int n;
+    cout<<"enter n :";
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Abhishek";
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    printfun();
+
+    return 0;
+}
